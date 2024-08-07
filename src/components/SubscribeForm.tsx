1d77b2852cf748ec3fc5e46657f9f8105ff771cf
giorgio-villani <user01@DESKTOP-UJDVA60.>
2024-08-07 15:21:16 -0500

    adjusted subscription to use formspree

--- src/components/SubscribeForm.tsx
+++ src/components/SubscribeForm.tsx
@@ -3,30 +3,28 @@
 import React, { useState } from 'react'
 
 const SubscribeForm: React.FC = () => {
-  const [email, setEmail] = useState('')
   const [status, setStatus] = useState('')
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
     const form = e.currentTarget
 
-    const formData = new FormData(form)
-    const formDataEntries = Array.from(formData.entries()).reduce(
-      (acc, [key, value]) => {
-        acc[key] = value as string
-        return acc
+    fetch(form.action, {
+      method: form.method,
+      body: new FormData(form),
+      headers: {
+        Accept: 'application/json',
       },
-      {} as Record<string, string>
-    )
-    const urlSearchParams = new URLSearchParams(formDataEntries).toString()
-
-    fetch('/', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-      body: urlSearchParams,
     })
-      .then(() => setStatus('success'))
-      .catch((error) => setStatus('error'))
+      .then((response) => {
+        if (response.ok) {
+          setStatus('success')
+          form.reset()
+        } else {
+          setStatus('error')
+        }
+      })
+      .catch(() => setStatus('error'))
   }
 
   return (
@@ -38,18 +36,14 @@ const SubscribeForm: React.FC = () => {
             Stay in the loop with everything you need to know.
           </p>
           <form
-            name="subscribe"
+            action="https://formspree.io/f/xwpebvon"
             method="POST"
-            data-netlify="true"
             onSubmit={handleSubmit}
           >
-            <input type="hidden" name="form-name" value="subscribe" />
             <input
               type="email"
               name="email"
               placeholder="Enter email here..."
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
               className="border border-red-500 p-2 mb-4 w-full rounded"
               required
             />
