bf151c790557f06eb4a8187148a75ac437047545
giorgio-villani <user01@DESKTOP-UJDVA60.>
2024-08-08 15:41:48 -0500

    adjusted metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,40 +3,42 @@ import { Inter } from 'next/font/google'
 import '@/styles/globals.css'
 import Header from '@/components/Header'
 import Footer from '@/components/Footer'
+import Head from 'next/head'
 
 const inter = Inter({ subsets: ['latin'] })
 
-// export const metadata: Metadata = {
-//   title: 'Forgn Studio',
-//   description: 'Learn Skills, Make Art and Meet People',
-// }
-
-export const metadata: Metadata = {
-  title: 'Forgn Studio',
-  description: 'Learn Skills, Make Art and Meet People',
-  openGraph: {
-    title: 'Forgn Studio',
-    description: 'Learn Skills, Make Art and Meet People',
-    url: 'https://forgn.art',
-    type: 'website',
-    images: [
-      {
-        url: 'forgn.png',
-        width: 1956,
-        height: 448,
-        alt: 'Forgn Studio Logo',
-      },
-    ],
-  },
-}
-
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode
-}>) {
+}) {
   return (
     <html lang="en">
+      <Head>
+        <title>Forgn Studio</title>
+        <meta
+          name="description"
+          content="Learn Skills, Make Art and Meet People"
+        />
+        <meta property="og:title" content="Forgn Studio" />
+        <meta
+          property="og:description"
+          content="Learn Skills, Make Art and Meet People"
+        />
+        <meta property="og:url" content="https://forgn.art" />
+        <meta property="og:type" content="website" />
+        <meta property="og:image" content="https://forgn.art/forgn.png" />
+        <meta property="og:image:width" content="1956" />
+        <meta property="og:image:height" content="448" />
+        <meta property="og:image:alt" content="Forgn Studio Logo" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Forgn Studio" />
+        <meta
+          name="twitter:description"
+          content="Learn Skills, Make Art and Meet People"
+        />
+        <meta name="twitter:image" content="https://forgn.art/forgn.png" />
+      </Head>
       <body className={inter.className}>
         <Header />
         {children}
