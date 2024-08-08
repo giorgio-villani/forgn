1f932366cc11d781ee3a600907e182c6f1d1a85a
giorgio-villani <user01@DESKTOP-UJDVA60.>
2024-08-08 18:35:47 -0500

    updated workshop details

--- src/app/workshops/[workshopId]/page.tsx
+++ src/app/workshops/[workshopId]/page.tsx
@@ -1,4 +1,5 @@
-import { useRouter } from 'next/router'
+import { notFound, useParams } from 'next/navigation'
+import workshops from '@/data/workshops'
 
 interface ClassDetailsProps {
   params: {
@@ -7,9 +8,40 @@ interface ClassDetailsProps {
 }
 
 export default function ClassDetails({ params }: ClassDetailsProps) {
+  const { workshopId } = params
+
+  const workshop = workshops.find((workshop) => workshop.id === workshopId)
+
+  if (!workshop) {
+    return notFound()
+  }
+
   return (
-    <h1 className="flex justify-center">
-      Details About Class {params.workshopId}
-    </h1>
+    <div className="max-w-screen-xl mx-auto font-poppins p-4">
+      <h1 className="text-5xl text-center font-inter mb-8">{workshop.title}</h1>
+      <div className="flex flex-col md:flex-row items-center">
+        <div className="w-full md:w-1/2 p-4">
+          <img
+            src={workshop.image}
+            className="w-full h-auto object-cover max-w-full max-h-[500px] object-cover"
+            alt={workshop.title}
+          />
+        </div>
+        <div className="w-full md:w-1/2 flex p-4">
+          <div className="px-5">
+            <h2 className="text-3xl mb-4">Instructor: {workshop.instructor}</h2>
+            <p className="text-lg leading-relaxed mb-4">
+              {workshop.description}
+            </p>
+            <p className="text-lg mb-4">
+              <strong>Sessions:</strong> {workshop.sessions}
+            </p>
+            <p className="text-lg mb-4">
+              <strong>Location:</strong> {workshop.location}
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
--- src/data/workshops.tsx
+++ src/data/workshops.tsx
@@ -1,41 +1,56 @@
 const workshops = [
   {
+    id: '0',
     title: 'Sculpture Class - Terracotta',
+    instructor: 'Shelley Townsend',
     description:
       'In this 4-week class, students will learn the basic tools and materials used to create hand-built ceramics using terracotta. Skills covered include pinching, coiling with a banding wheel, slab preparation with a slab roller, wedging, and glazing.',
+    location: 'East End Maker Hub - Y114',
     sessions: 'Next Session: Aug 5, 12, 19, 26',
     price: '$350',
     image: '/classes/sculpture-terracotta.jpg',
   },
   {
+    id: '1',
     title: 'Sculpture Class - Polymer Clay',
+    instructor: 'Bailey Ryherd',
     description:
       'This 4-week class introduces students to polymer clay techniques. You will learn to condition, blend, shape, and bake polymer clay, creating various small sculptures and decorative items. Projects will be guided step-by-step, ensuring you complete several finished pieces.',
+    location: 'East End Maker Hub - Y114',
     sessions: 'Next Session: Aug 6, 13, 20, 27',
     price: '$250',
     image: '/classes/polymer.jpg',
   },
   {
+    id: '2',
     title: 'Digital Art: 101',
+    instructor: 'Olesia Y. Rodionova',
     description:
       'In this 4-week class, students will learn the fundamentals of digital art. Topics include using digital art software, understanding layers and tools, and creating digital illustrations and paintings. No prior experience is required.',
-    sessions: 'Next Session: Aug 7, 14, 21, 28',
+    location: 'East End Maker Hub - Y114',
+    sessions: 'Next Session: Aug 6, 13, 20, 27',
     price: '$220',
     image: '/classes/digital-art.jpg',
   },
   {
+    id: '3',
     title: 'Video Editing: DaVinci',
+    instructor: 'Thomas MacDonald',
     description:
       'This 4-week class covers the basics of video editing using DaVinci Resolve. Students will learn how to import footage, edit clips, add transitions and effects, and export their projects. Ideal for beginners looking to get started with professional video editing software.',
-    sessions: 'Next Session: Aug 7, 14, 21, 28',
+    location: 'East End Maker Hub - Y114',
+    sessions: 'Next Session: Aug 6, 13, 20, 27',
     price: '$150',
     image: '/classes/davinci.jpg',
   },
   {
+    id: '4',
     title: 'AI Art: Stable Diffusion',
+    instructor: 'Giorgio Villani',
     description:
       'A Stable Diffusion class focused on Automatic 1111 Stable Diffusion would cover the usage and customization of the popular AI image generation tool. The course would include installing and setting up the Automatic 1111 interface, understanding model parameters, exploring various prompts and styles, and fine-tuning model outputs. Students would learn to generate high-quality images, apply custom settings, and utilize extensions and scripts to enhance their workflow.',
-    sessions: 'Next Session: Aug 7, 14, 21, 28',
+    location: 'East End Maker Hub - Y114',
+    sessions: 'Next Session: Aug 6, 13, 20, 27',
     price: '$150',
     image: '/classes/ai.gif',
   },
