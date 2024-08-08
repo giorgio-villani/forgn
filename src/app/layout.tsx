8a0ba9b146ea2ac75e42d898c2c78b96bd020420
giorgio-villani <user01@DESKTOP-UJDVA60.>
2024-08-08 15:21:12 -0500

    updated metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,9 +6,28 @@ import Footer from '@/components/Footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
+// export const metadata: Metadata = {
+//   title: 'Forgn Studio',
+//   description: 'Learn Skills, Make Art and Meet People',
+// }
+
 export const metadata: Metadata = {
   title: 'Forgn Studio',
   description: 'Learn Skills, Make Art and Meet People',
+  openGraph: {
+    title: 'Forgn Studio',
+    description: 'Learn Skills, Make Art and Meet People',
+    url: 'https://forgn.art',
+    type: 'website',
+    images: [
+      {
+        url: 'forgn.png',
+        width: 1956,
+        height: 448,
+        alt: 'Forgn Studio Logo',
+      },
+    ],
+  },
 }
 
 export default function RootLayout({
