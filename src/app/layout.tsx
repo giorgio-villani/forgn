4f19b4e15ed7037bffee4a24086507032ef252eb
giorgio-villani <user01@DESKTOP-UJDVA60.>
2024-08-08 15:54:51 -0500

    adjusted metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,9 +27,12 @@ export default function RootLayout({
         />
         <meta property="og:url" content="https://forgn.art" />
         <meta property="og:type" content="website" />
-        <meta property="og:image" content="https://forgn.art/forgn.png" />
-        <meta property="og:image:width" content="1956" />
-        <meta property="og:image:height" content="448" />
+        <meta
+          property="og:image"
+          content="https://forgn.art/forgn_metadata.png"
+        />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
         <meta property="og:image:alt" content="Forgn Studio Logo" />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:title" content="Forgn Studio" />
@@ -37,7 +40,10 @@ export default function RootLayout({
           name="twitter:description"
           content="Learn Skills, Make Art and Meet People"
         />
-        <meta name="twitter:image" content="https://forgn.art/forgn.png" />
+        <meta
+          name="twitter:image"
+          content="https://forgn.art/forgn_metadata.png"
+        />
       </Head>
       <body className={inter.className}>
         <Header />
