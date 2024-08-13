bbb3a07a037518cc895cb1fd3cf633d99eae6683
giorgio-villani <user01@DESKTOP-UJDVA60.>
2024-08-13 13:16:21 -0500

    adjusted metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,9 +3,35 @@ import { Inter } from 'next/font/google'
 import '@/styles/globals.css'
 import Header from '@/components/Header'
 import Footer from '@/components/Footer'
-import Head from 'next/head'
 
 const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Forgn Studio',
+  description: 'Learn Skills, Make Art and Meet People',
+  keywords:
+    'art, workshops, teachers, school, learning, art classes, sculpture, digital art, painting, drawing, creative classes, art education, Houston art studio, art lessons, art events, art exhibitions, creative community, art techniques, artistic skills, art for kids, art for adults, art courses, visual arts',
+  openGraph: {
+    title: 'Forgn Studio',
+    description: 'Learn Skills, Make Art and Meet People',
+    url: 'https://forgn.art',
+    type: 'website',
+    images: [
+      {
+        url: 'https://forgn.art/forgn_metadata.png',
+        width: 1200,
+        height: 630,
+        alt: 'Forgn Studio Logo',
+      },
+    ],
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Forgn Studio',
+    description: 'Learn Skills, Make Art and Meet People',
+    images: ['https://forgn.art/forgn_metadata.png'],
+  },
+}
 
 export default function RootLayout({
   children,
@@ -14,41 +40,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <Head>
-        <title>Forgn Studio</title>
-        <meta
-          name="description"
-          content="Learn Skills, Make Art and Meet People"
-        />
-        <meta
-          name="keywords"
-          content="art, workshops, teachers, school, learning, art classes, sculpture, digital art, painting, drawing, creative classes, art education, Houston art studio, art lessons, art events, art exhibitions, creative community, art techniques, artistic skills, art for kids, art for adults, art courses, visual arts"
-        />
-        <meta property="og:title" content="Forgn Studio" />
-        <meta
-          property="og:description"
-          content="Learn Skills, Make Art and Meet People"
-        />
-        <meta property="og:url" content="https://forgn.art" />
-        <meta property="og:type" content="website" />
-        <meta
-          property="og:image"
-          content="https://forgn.art/forgn_metadata.png"
-        />
-        <meta property="og:image:width" content="1200" />
-        <meta property="og:image:height" content="630" />
-        <meta property="og:image:alt" content="Forgn Studio Logo" />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content="Forgn Studio" />
-        <meta
-          name="twitter:description"
-          content="Learn Skills, Make Art and Meet People"
-        />
-        <meta
-          name="twitter:image"
-          content="https://forgn.art/forgn_metadata.png"
-        />
-      </Head>
       <body className={inter.className}>
         <Header />
         {children}
