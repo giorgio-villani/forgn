0a064df11bc2f43edb24686875ddca0d9114277e
giorgio-villani <giorgio-villani@users.noreply.github.com>
2024-11-01 22:28:29 -0500

    updated redirect for /

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/',
+        has: [
+          {
+            type: 'host',
+            value: 'forgn.art', // Redirects from non-www to www
+          },
+        ],
+        destination: 'https://www.forgn.art/',
+        permanent: true,
+      },
+    ]
+  },
+}
 
-export default nextConfig;
+export default nextConfig
