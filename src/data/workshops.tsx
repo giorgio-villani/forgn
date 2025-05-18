d2e23221e6c3ae57daf10a4040805017a699bc39
giorgio-villani <giorgio-villani@users.noreply.github.com>
2025-05-18 12:56:12 -0500

    updated link

--- src/data/workshops.tsx
+++ src/data/workshops.tsx
@@ -12,8 +12,8 @@ const workshops = [
     price: '$400',
     discountedPrice: '$300',
     image: '/classes/sculpture-terracotta.webp',
-    booking: 'https://www-1577h.bookeo.com/bookeo/b_forgnstudio_start.html',
-    discount_booking: 'https://www-1577h.bookeo.com/bookeo/b_forgnstudio_start.html'
+    booking: 'https://bookeo.com/forgnstudio',
+    discount_booking: 'https://bookeo.com/forgnstudio'
   },
   {
     id: '1',
