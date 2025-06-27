e39675aad5c11198c87236e17e8f5ee9dfb1a34d
giorgio-villani <giorgio-villani@users.noreply.github.com>
2025-06-27 09:54:32 -0500

    updated to tag manager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -72,21 +72,29 @@ export default function RootLayout({
           href="/apple-touch-icon.png"
           sizes="180x180"
         />
-        {/* Google Analytics */}
-        <Script
-          src="https://www.googletagmanager.com/gtag/js?id=G-B56MTPSQ10"
-          strategy="afterInteractive"
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-NVP7C6MS');`,
+          }}
         />
-        <Script id="google-analytics" strategy="afterInteractive">
-          {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'G-B56MTPSQ10');
-        `}
-        </Script>
+        {/* End Google Tag Manager */}
       </head>
       <body className={`${inter.className} min-h-screen flex flex-col`}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NVP7C6MS"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         <Header />
         <div className="grow">{children}</div>
         <Footer />
