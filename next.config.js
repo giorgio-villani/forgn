37ac81188955daaa17cf5748126ad5ccebb7802b
giorgio-villani <giorgio-villani@users.noreply.github.com>
2025-09-12 13:49:08 -0500

    updated to redirect to admin page

--- next.config.js
+++ next.config.js
@@ -1,5 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'ambitious-cactus-1589a7699f.media.strapiapp.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
   async redirects() {
     return [
       { source: '/workshops/0', destination: '/workshops/sculpture-class-terracotta', permanent: true },
@@ -9,6 +19,7 @@ const nextConfig = {
       { source: '/workshops/4', destination: '/workshops/ai-art-stable-diffusion', permanent: true },
       { source: '/workshops/5', destination: '/workshops/website-design-figma', permanent: true },
       { source: '/workshops/6', destination: '/workshops/blender-3d-sculpting', permanent: true },
+      { source: '/admin', destination: 'https://ambitious-cactus-1589a7699f.strapiapp.com/admin', permanent: true },
     ]
   },
 }
